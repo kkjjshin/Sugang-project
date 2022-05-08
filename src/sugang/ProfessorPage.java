6d32f18c5b2116d1da30c77eddfc8eca42950a40
hjeonglee <wer5645@g.skku.edu>
2022-05-09 00:16:08 +0900

    skeleton code commit

---
+++ src/sugang/ProfessorPage.java
@@ -0,0 +1,13 @@
+package sugang;
+
+public class ProfessorPage {
+	
+	public void showSubject() {};
+	//교수의 강의 목록 보여주기(학수번호, 과목명, 학생이름, 수강사유)
+	public void goProSugang () {};
+	//허용 누르면 이동
+	
+	//DB 
+		//1. 강의 과목 목록에서 해당 교수 과목 읽어오기
+		//2. 전체 학생 대기과목에서 해당 교수 과목 읽어오기
+}
---
+++ src/sugang/Schedule.java
@@ -0,0 +1,9 @@
+package sugang;
+
+public class Schedule {
+	//나의 시간표 보여주기
+	public void showTimetable() {};
+	
+	//DB 설명
+		//학생 확정 과목에서 데이터 읽어오기
+}
