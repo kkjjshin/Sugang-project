2480a37710763daccc99326752803b68d05ddc03
Kyungjune Shin <rudwns2832@naver.com>
2022-05-08 17:41:42 +0900

    er

--- src/sugang/Stest.java
+++ src/sugang/Stest.java
@@ -4,7 +4,7 @@ public class Stest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ex입니다.33");
+		System.out.println("ex입니다.3dd3");
 	}
 
 }
